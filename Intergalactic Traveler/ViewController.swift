befe88846a4bebad376bc788b8e8da43c09d676b
Grant Spraker <grant.spraker@gmail.com>
2021-10-20 10:05:24 -0600

    Got the background to work withe the Buttons and looks nice (to me)

--- Intergalactic Traveler/ViewController.swift
+++ Intergalactic Traveler/ViewController.swift
@@ -7,13 +7,81 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// Button Corner Radius and Border code from StackOverflow.com
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+@IBDesignable extension UIButton
+{
+
+    @IBInspectable var borderWidth: CGFloat
+    {
+        set
+        {
+            layer.borderWidth = newValue
+        }
+        get
+        {
+            return layer.borderWidth
+        }
     }
 
+    @IBInspectable var cornerRadius: CGFloat
+    {
+        set
+        {
+            layer.cornerRadius = newValue
+        }
+        get
+        {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor?
+    {
+        set
+        {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get
+        {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
+
+class ViewController: UIViewController
+{
+
+    
+    override func viewDidLoad()
+    {
+              super.viewDidLoad()
+                 assignbackground()
+        
+        // From StackOverflow.com (for the background)
+        func assignbackground()
+      {
+          let background = UIImage(named: "Space-Bg")
+
+          var imageView : UIImageView!
+              imageView = UIImageView(frame: view.bounds)
+          imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+              imageView.clipsToBounds = true
+              imageView.image = background
+              imageView.center = view.center
+              view.addSubview(imageView)
+              self.view.sendSubviewToBack(imageView)
+          
+//              let randomNumber = Int.random(in: 1...3)
+//              starImageView.image = UIImage(named: "\(starName)\(randomNumber)")
+      }
+    }
+
+      
+    
 
 }
+    
 
---
+++ StarViewColtrollerViewController.swift
@@ -0,0 +1,31 @@
+//
+//  StarViewColtrollerViewController.swift
+//  Intergalactic Traveler
+//
+//  Created by Grant Spraker on 10/18/21.
+//
+
+import UIKit
+
+class StarViewColtrollerViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
