b173f4ac52adbf74bdd426cd6d42b2531f48a837
Grant Spraker <grant.spraker@gmail.com>
2021-10-21 11:00:34 -0600

    MVP is done (thank god)

--- Intergalactic Traveler/StarController.swift
+++ Intergalactic Traveler/StarController.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class StarController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    @IBOutlet weak var starImage: UIImageView!
+    
+    var starName = ""
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        starImage.image = UIImage(named: starName)
+        print(starName)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Intergalactic Traveler/ViewController.swift
+++ Intergalactic Traveler/ViewController.swift
@@ -7,23 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        assignbackground()
+        
+       
     }
     
+    func assignbackground(){
+            let background = UIImage(named: "Space-Bg")
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+            var imageView : UIImageView!
+            imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+            imageView.clipsToBounds = true
+            imageView.image = background
+            imageView.center = view.center
+            view.addSubview(imageView)
+            self.view.sendSubviewToBack(imageView)
+        }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        let nextViewController = segue.destination as! StarController
+        
+        let randomNumber = Int.random(in: 1...3)
+        
+        
+        if segue.identifier == "redDwarf"
+        {
+            nextViewController.starName = "redDwarf\(randomNumber)"
+        }
+        else
+        {
+            nextViewController.starName = "blueDwarf\(randomNumber)"
+        }
     }
-    */
+   
 
 }
